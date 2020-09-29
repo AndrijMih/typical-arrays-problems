f77e2da83becf2eb0672b0a786307eaf6775b269
andrijmih <an12dre34@rambler.ru>
2020-09-29 12:37:11 +0300

    feat: add avg

--- src/index.js
+++ src/index.js
@@ -25,3 +25,15 @@ exports.max = function max(array) {
     }
     return max;
 }
+
+exports.avg = function avg(array) {
+    if (arguments.length === 0 || array.length === 0) {
+        return 0;
+    }
+
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+    }
+    return sum / array.length;
+}
