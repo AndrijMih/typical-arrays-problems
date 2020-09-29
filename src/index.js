f05e10a3be8b01a355b9499860b62f37019a1600
andrijmih <an12dre34@rambler.ru>
2020-09-29 12:35:35 +0300

    feat: add min

--- src/index.js
+++ src/index.js
@@ -11,29 +11,3 @@ exports.min = function min(array) {
     }
     return min;
 }
-
-exports.max = function max(array) {
-    if (arguments.length === 0 || array.length === 0) {
-        return 0;
-    }
-
-    let max = array[0];
-    for (let i = 1; i < array.length; i++) {
-        if (array[i] > max) {
-            max = array[i];
-        }
-    }
-    return max;
-}
-
-exports.avg = function avg(array) {
-    if (arguments.length === 0 || array.length === 0) {
-        return 0;
-    }
-
-    let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-        sum = sum + array[i];
-    }
-    return sum / array.length;
-}
